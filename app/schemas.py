0783559238258f03ea7906b77f4990930d0e4f5b
HarperCodeLabs <louisemily640@gmail.com>
2026-05-10 09:22:57 -0700

    fix/change ChatResponse type in schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
 
+
 class ChatRequest(BaseModel):
     message: str
 
-class ChatResponse(BaseModel):
-    reply = str
 
+class ChatResponse(BaseModel):
+    reply: str
